fdb83062557d9ce3b9fe84f41aad73330d34f2fb
tech180 <rileylawson00@gmail.com>
2026-04-16 20:50:10 -0500

    0.0.2 - turned all trainers and routes to json when building

--- trainer_rules.mk
+++ trainer_rules.mk
@@ -7,5 +7,10 @@ AUTO_GEN_TARGETS += test/battle/trainer_control.h
 AUTO_GEN_TARGETS += test/battle/partner_control.h
 AUTO_GEN_TARGETS += src/data/debug_trainers.h
 
+TRAINER_JSONS := $(shell find data/trainers -name "*.json")
+
+src/data/trainers.h: $(TRAINER_JSONS) scripts/build_data.py
+	python3 scripts/build_data.py
+
 %.h: %.party
 	$(CPP) $(CPPFLAGS) -traditional-cpp - < $< | $(TRAINERPROC) -o $@ -i $< -
